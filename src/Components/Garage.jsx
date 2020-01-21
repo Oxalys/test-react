7d825a784c5bce6a329d01531198673f51756004
Oxalys <Oxalys@MacBook-Pro-de-Salome.local>
2020-01-21 12:40:46 +0100

    exo border fait

--- src/Components/Garage.jsx
+++ src/Components/Garage.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Vehicule from './Vehicule';
 
 
 const Garage = () => {
-    return(
-        <div></div>
+    return (
+        <div>
+            <Vehicule nom="Mercedes Benz S 260" année="1991" couleur="bleue" carburant="essence" etat="non validé" />
+            <br/>
+            <Vehicule nom="Fiat 500" année="2010" couleur="noire" carburant="essence" etat="non validé" />
+            <br/>
+            <Vehicule nom="Volkswagen Crafter" année="2016" couleur="blanche" carburant="diesel" etat="validé" />
+            <br/>
+            <Vehicule nom="BMV 114" année="2012" couleur="bleue" carburant="diesel" etat="non validé" />
+            <br/>
+            <Vehicule nom="Audi A3" année="2015" couleur="rouge" carburant="diesel" etat="validé" />
+        </div>
     )
 }
 
--- src/Components/Vehicule.jsx
+++ src/Components/Vehicule.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 
 
 const Vehicule = (props) => {
+
+
     return(
-        <div>{props.nom}, {props.année}, {props.couleur}, {props.carburant}, {props.etat}</div>
+        <div>{props.nom}, {props.année}, {props.couleur},             {props.carburant}, <div className={props.etat === "non validé" ? "border-bottom border-danger" : ""} >{props.etat}</div>
+        </div>
     )
 }
 
